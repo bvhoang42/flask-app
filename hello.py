3d27c993b0378d1395afafa8bfe424ba284a195b
bvhoang42 <67403811+bvhoang42@users.noreply.github.com>
2020-06-25 11:18:00 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask import make_responseapp = Flask(__name__)@app.route('/')
-def home():
-    return "Hello World!"@app.route('/<page_name>')
-def other_page(page_name):
-    response = make_response('The page named %s does not exist.' \
-                             % page_name, 404)
-    return responseif __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
